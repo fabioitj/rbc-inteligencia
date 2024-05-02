4194e6eb6493c54c718fb3f284a6b44c5cd8a4cb
Fabio <fridow8@gmail.com>
2024-05-02 14:44:46 -0300

    Back-end finished

--- src/server.ts
+++ src/server.ts
@@ -16,113 +16,194 @@ console.log("Conectou no banco de dados!");
 app.use(cors());
 app.use(bodyParser.json());
 
-  app.post('/casos', async (req: Request, res: Response) => {
-    try {
-      const {
-        platform,
-        gamemode,
-        mapname,
-        roundnumber,
-        objectivelocation,
-        endroundreason,
-        roundduration,
-        clearencelevel,
-        skillrank,
-        role,
-        haswon,
-        operator,
-        nbkills,
-        isdead
-      } = req.body;
+app.post('/casos', async (req: Request, res: Response) => {
+	try {
+		const {
+		platform,
+		gamemode,
+		mapname,
+		roundnumber,
+		objectivelocation,
+		endroundreason,
+		roundduration,
+		clearencelevel,
+		skillrank,
+		role,
+		haswon,
+		operator,
+		nbkills,
+		isdead
+		} = req.body;
 
-      console.log(req.body);
-  
-      if (!platform || !gamemode || !mapname || !roundnumber || !objectivelocation || !endroundreason || !roundduration || !clearencelevel || !skillrank || !role || (haswon !== 0 && haswon !== 1) || !operator || !nbkills || (isdead !== 0 && isdead !== 1)) {
-        return res.status(400).send({ error: 'Todos os campos são obrigatórios.' });
-      }
-  
-      const matchModel = new Match({
-        platform,
-        gamemode,
-        mapname,
-        roundnumber,
-        objectivelocation,
-        endroundreason,
-        roundduration,
-        clearencelevel,
-        skillrank,
-        role,
-        haswon,
-        operator,
-        nbkills,
-        isdead
-      });
-  
-      await matchModel.save();
-  
-      res.status(201).send(matchModel);
-    } catch (error) {
-      console.error('Erro ao adicionar caso:', error);
-      res.status(500).send({ error: 'Erro interno ao adicionar caso.' });
-    }
-  });
+		if (!platform || !gamemode || !mapname || !roundnumber || !objectivelocation || !endroundreason || !roundduration || !clearencelevel || !skillrank || !role || (haswon !== 0 && haswon !== 1) || !operator || !nbkills || (isdead !== 0 && isdead !== 1)) {
+			return res.status(400).send({ error: 'Todos os campos são obrigatórios.' });
+		}
 
-  app.get('/casos/:id', async (req: Request, res: Response) => {
-      try {
-      const casoBase = await Match.findById(req.params.id);
-  
-      if (!casoBase) {
-          return res.status(404).send({ error: 'Caso médico não encontrado.' });
-      }
-  
-      const casos = await Match.find();
+		const matchModel = new Match({
+			platform,
+			gamemode,
+			mapname,
+			roundnumber,
+			objectivelocation,
+			endroundreason,
+			roundduration,
+			clearencelevel,
+			skillrank,
+			role,
+			haswon,
+			operator,
+			nbkills,
+			isdead
+		});
 
-      const casosParaComparar = casos.filter(caso => caso._id.toString() !== casoBase._id.toString());
+		await matchModel.save();
 
-      const casosComSimilaridade = casosParaComparar.map((caso: Match) => ({
-          caso,
-          similaridade: calculateSimilarity(casoBase, caso),
-      }));
-  
-      casosComSimilaridade.sort((a, b) => b.similaridade - a.similaridade);
-  
-      res.status(200).send(casosComSimilaridade.slice(0, 20));
-      } catch (error) {
-      console.error('Erro ao buscar casos médicos:', error);
-      res.status(500).send({ error: 'Erro interno ao buscar casos médicos.' });
-      }
-  });
+		res.status(201).send(matchModel);
+	} catch (error) {
+		console.error('Erro ao adicionar caso:', error);
+		res.status(500).send({ error: 'Erro interno ao adicionar caso.' });
+	}
+});
 
-  app.put('/casos', async (req: Request, res: Response) => {
-    try {
-      const updateResult = await Match.updateMany({}, {
-        $set: {
-          weights: {
-            platform: 0.05,
-            gamemode: 0.1,
-            mapname: 0.1,
-            roundnumber: 0.03,
-            objectivelocation: 0.02,
-            endroundreason: 0.1,
-            roundduration: 0.03,
-            clearencelevel: 0.02,
-            skillrank: 0.2,
-            role: 0.1,
-            haswon: 0.1,
-            operator: 0.1,
-            nbkills: 0.03,
-            isdead: 0.02
-          }
-        }
-      });
-  
-      console.log(`Updated ${updateResult.modifiedCount} documents`);
-    } catch (error) {
-      console.error('Failed to update weights:', error);
-    }
-    res.status(200).json({success: true});
-  });
+app.get('/casos/:id', async (req: Request, res: Response) => {
+	try {
+		const casoBase = await Match.findById(req.params.id);
+
+		if (!casoBase) {
+			return res.status(404).send({ error: 'Caso médico não encontrado.' });
+		}
+
+		const casos = await Match.find().limit(100000);
+
+		const casosParaComparar = casos.filter(caso => caso._id.toString() !== casoBase._id.toString());
+
+		const casosComSimilaridade = casosParaComparar.map((caso: Match) => ({
+			caso,
+			similaridade: calculateSimilarity(<MatchModel>{
+				platform: casoBase.platform,
+				gamemode: casoBase.gamemode,
+				mapname: casoBase.mapname,
+				roundnumber: casoBase.roundnumber,
+				objectivelocation: casoBase.objectivelocation,
+				endroundreason: casoBase.endroundreason,
+				roundduration: casoBase.roundduration,
+				clearencelevel: casoBase.clearencelevel,
+				skillrank: casoBase.skillrank,
+				role: casoBase.role,
+				haswon: casoBase.haswon,
+				operator: casoBase.operator,
+				nbkills: casoBase.nbkills,
+				isdead: casoBase.isdead,
+				weights: casoBase.weights
+			}, <MatchModel>{
+				platform: caso.platform,
+				gamemode: caso.gamemode,
+				mapname: caso.mapname,
+				roundnumber: caso.roundnumber,
+				objectivelocation: caso.objectivelocation,
+				endroundreason: caso.endroundreason,
+				roundduration: caso.roundduration,
+				clearencelevel: caso.clearencelevel,
+				skillrank: caso.skillrank,
+				role: caso.role,
+				haswon: caso.haswon,
+				operator: caso.operator,
+				nbkills: caso.nbkills,
+				isdead: caso.isdead,
+				weights: caso.weights
+			}),
+		}));
+
+		casosComSimilaridade.sort((a, b) => b.similaridade - a.similaridade);
+
+		res.status(200).send(casosComSimilaridade.slice(0, 20));
+	} catch (error) {
+		console.error('Erro ao buscar casos médicos:', error);
+		res.status(500).send({ error: 'Erro interno ao buscar casos médicos.' });
+	}
+});
+
+app.post('/casos/data', async (req: Request, res: Response) => {
+	try {
+		const {
+			platform,
+			gamemode,
+			mapname,
+			roundnumber,
+			objectivelocation,
+			endroundreason,
+			roundduration,
+			clearencelevel,
+			skillrank,
+			role,
+			haswon,
+			operator,
+			nbkills,
+			isdead
+		} = req.body;
+
+		const casos = await Match.find().limit(100000);
+
+		const casosComSimilaridade = casos.map((caso: Match) => ({
+			caso,
+			similaridade: calculateSimilarity(<MatchModel>{
+				platform,
+				gamemode,
+				mapname,
+				roundnumber,
+				objectivelocation,
+				endroundreason,
+				roundduration,
+				clearencelevel,
+				skillrank,
+				role,
+				haswon,
+				operator,
+				nbkills,
+				isdead,
+				weights: {
+					platform: 0.05,
+					gamemode: 0.1,
+					mapname: 0.1,
+					roundnumber: 0.03,
+					objectivelocation: 0.02,
+					endroundreason: 0.1,
+					roundduration: 0.03,
+					clearencelevel: 0.02,
+					skillrank: 0.2,
+					role: 0.1,
+					haswon: 0.1,
+					operator: 0.1,
+					nbkills: 0.03,
+					isdead: 0.02
+				}
+			}, <MatchModel>{
+				platform: caso.platform,
+				gamemode: caso.gamemode,
+				mapname: caso.mapname,
+				roundnumber: caso.roundnumber,
+				objectivelocation: caso.objectivelocation,
+				endroundreason: caso.endroundreason,
+				roundduration: caso.roundduration,
+				clearencelevel: caso.clearencelevel,
+				skillrank: caso.skillrank,
+				role: caso.role,
+				haswon: caso.haswon,
+				operator: caso.operator,
+				nbkills: caso.nbkills,
+				isdead: caso.isdead,
+				weights: caso.weights
+			}),
+		}));
+
+		casosComSimilaridade.sort((a, b) => b.similaridade - a.similaridade);
+
+		res.status(200).send(casosComSimilaridade.slice(0, 20));
+	} catch (error) {
+		console.error('Failed to update weights:', error);
+	}
+});
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+	console.log(`Servidor rodando na porta ${port}`);
 });
--- src/utils/SimilaridadePonderada.ts
+++ src/utils/SimilaridadePonderada.ts
@@ -1,3 +1,21 @@
+interface WeightModel {
+    platform: number;
+    gamemode: number;
+    mapname: number;
+    roundnumber: number;
+    objectivelocation: number;
+    endroundreason: number;
+    roundduration: number;
+    clearencelevel: number;
+    skillrank: number;
+    role: number;
+    haswon: number;
+    operator: number;
+    nbkills: number;
+    isdead: number;
+    [key: string]: any;
+  }
+
 interface MatchModel {
     platform: string;
     gamemode: string;
@@ -13,20 +31,21 @@ interface MatchModel {
     operator: string;
     nbkills: number;
     isdead: number;
+    weights: WeightModel;
     [key: string]: any;
 }
 
 const mapCorrelation: {[key: string]: any} = {
-    "BANK": { "CLUB_HOUSE": 0.8, "OREGON": 0.4, "CHALET": 0, "CONSULATE": 0, "HEREFORD_BASE": 0, "HOUSE": 0, "KAFE_DOSTOYEVSKY": 0, "PLANE": 0, "KANAL": 0 },
-    "CHALET": { "BANK": 0, "CLUB_HOUSE": 0, "OREGON": 0, "CONSULATE": 0, "HEREFORD_BASE": 0, "HOUSE": 0, "KAFE_DOSTOYEVSKY": 0, "PLANE": 0, "KANAL": 0 },
-    "CLUB_HOUSE": { "BANK": 0.8, "OREGON": 0.5, "CHALET": 0, "CONSULATE": 0, "HEREFORD_BASE": 0, "HOUSE": 0, "KAFE_DOSTOYEVSKY": 0, "PLANE": 0, "KANAL": 0 },
-    "CONSULATE": { "BANK": 0, "CHALET": 0, "CLUB_HOUSE": 0, "OREGON": 0, "HEREFORD_BASE": 0, "HOUSE": 0, "KAFE_DOSTOYEVSKY": 0, "PLANE": 0, "KANAL": 0 },
-    "HEREFORD_BASE": { "BANK": 0, "CHALET": 0, "CLUB_HOUSE": 0, "CONSULATE": 0, "OREGON": 0, "HOUSE": 0, "KAFE_DOSTOYEVSKY": 0, "PLANE": 0, "KANAL": 0 },
-    "HOUSE": { "BANK": 0, "CHALET": 0, "CLUB_HOUSE": 0, "CONSULATE": 0, "HEREFORD_BASE": 0, "OREGON": 0, "KAFE_DOSTOYEVSKY": 0, "PLANE": 0, "KANAL": 0 },
-    "KAFE_DOSTOYEVSKY": { "BANK": 0, "CHALET": 0, "CLUB_HOUSE": 0, "CONSULATE": 0, "HEREFORD_BASE": 0, "HOUSE": 0, "OREGON": 0, "PLANE": 0, "KANAL": 0 },
-    "OREGON": { "BANK": 0.4, "CHALET": 0, "CLUB_HOUSE": 0.5, "CONSULATE": 0, "HEREFORD_BASE": 0, "HOUSE": 0, "KAFE_DOSTOYEVSKY": 0, "PLANE": 0, "KANAL": 0 },
-    "PLANE": { "BANK": 0, "CHALET": 0, "CLUB_HOUSE": 0, "CONSULATE": 0, "HEREFORD_BASE": 0, "HOUSE": 0, "KAFE_DOSTOYEVSKY": 0, "OREGON": 0, "KANAL": 0 },
-    "KANAL": { "BANK": 0, "CHALET": 0, "CLUB_HOUSE": 0, "CONSULATE": 0, "HEREFORD_BASE": 0, "HOUSE": 0, "KAFE_DOSTOYEVSKY": 0, "PLANE": 0, "OREGON": 0 }
+    "BANK": { "BANK": 1, "CHALET": 0.6, "CLUB_HOUSE": 0.8, "CONSULATE": 0.7, "HEREFORD_BASE": 0.5, "HOUSE": 0.4, "KAFE_DOSTOYEVSKY": 0.6, "OREGON": 0.5, "PLANE": 0.3, "KANAL": 0.4 },
+    "CHALET": { "CHALET": 1, "BANK": 0.6, "CLUB_HOUSE": 0.7, "CONSULATE": 0.6, "HEREFORD_BASE": 0.4, "HOUSE": 0.3, "KAFE_DOSTOYEVSKY": 0.5, "OREGON": 0.4, "PLANE": 0.2, "KANAL": 0.3 },
+    "CLUB_HOUSE": { "CLUB_HOUSE": 1, "BANK": 0.8, "CHALET": 0.7, "CONSULATE": 0.8, "HEREFORD_BASE": 0.6, "HOUSE": 0.5, "KAFE_DOSTOYEVSKY": 0.7, "OREGON": 0.5, "PLANE": 0.4, "KANAL": 0.5 },
+    "CONSULATE": { "CONSULATE": 1, "BANK": 0.7, "CHALET": 0.6, "CLUB_HOUSE": 0.8, "HEREFORD_BASE": 0.4, "HOUSE": 0.3, "KAFE_DOSTOYEVSKY": 0.6, "OREGON": 0.3, "PLANE": 0.2, "KANAL": 0.3 },
+    "HEREFORD_BASE": { "HEREFORD_BASE": 1, "BANK": 0.5, "CHALET": 0.4, "CLUB_HOUSE": 0.6, "CONSULATE": 0.4, "HOUSE": 0.2, "KAFE_DOSTOYEVSKY": 0.4, "OREGON": 0.2, "PLANE": 0.1, "KANAL": 0.2 },
+    "HOUSE": { "HOUSE": 1, "BANK": 0.4, "CHALET": 0.3, "CLUB_HOUSE": 0.5, "CONSULATE": 0.3, "HEREFORD_BASE": 0.2, "KAFE_DOSTOYEVSKY": 0.3, "OREGON": 0.1, "PLANE": 0.1, "KANAL": 0.1 },
+    "KAFE_DOSTOYEVSKY": { "KAFE_DOSTOYEVSKY": 1, "BANK": 0.6, "CHALET": 0.5, "CLUB_HOUSE": 0.7, "CONSULATE": 0.6, "HEREFORD_BASE": 0.4, "HOUSE": 0.3, "OREGON": 0.4, "PLANE": 0.2, "KANAL": 0.3 },
+    "OREGON": { "OREGON": 1, "BANK": 0.5, "CHALET": 0.4, "CLUB_HOUSE": 0.5, "CONSULATE": 0.3, "HEREFORD_BASE": 0.2, "HOUSE": 0.1, "KAFE_DOSTOYEVSKY": 0.4, "PLANE": 0.1, "KANAL": 0.2 },
+    "PLANE": { "PLANE": 1, "BANK": 0.3, "CHALET": 0.2, "CLUB_HOUSE": 0.4, "CONSULATE": 0.2, "HEREFORD_BASE": 0.1, "HOUSE": 0.1, "KAFE_DOSTOYEVSKY": 0.2, "OREGON": 0.1, "KANAL": 0.1 },
+    "KANAL": { "KANAL": 1, "BANK": 0.4, "CHALET": 0.3, "CLUB_HOUSE": 0.5, "CONSULATE": 0.3, "HEREFORD_BASE": 0.2, "HOUSE": 0.1, "KAFE_DOSTOYEVSKY": 0.3, "OREGON": 0.2, "PLANE": 0.1 }
 };
 
 const operatorCorrelation: {[key: string]: string[]} = {
@@ -54,22 +73,24 @@ function getOperatorCorrelation(op1: string, op2: string): number {
 function calculateSimilarity(baseCase: MatchModel, itCase: MatchModel): number {
     let totalSimilarity = 0;
 
-    Object.keys(baseCase.weights).forEach(attribute => {
-        const weight = baseCase.weights[attribute];
+    Object.keys(baseCase).forEach(attribute => {
         const baseCaseValue = baseCase[attribute];
-        const itCaseValue = itCase[attribute];
-
-        let similarityAttribute = 0;
-
-        if (attribute === "mapname") {
-            similarityAttribute = mapCorrelation[baseCaseValue]?.[itCaseValue] || 0;
-        } else if (attribute === "operator") {
-            similarityAttribute = getOperatorCorrelation(baseCaseValue, itCaseValue);
-        } else {
-            similarityAttribute = baseCaseValue === itCaseValue ? 1 : 0;
+        if (attribute !== "weights" && baseCaseValue) {
+            const weight = itCase.weights[attribute];
+            const itCaseValue = itCase[attribute];
+    
+            let similarityAttribute = 0;
+    
+            if (attribute === "mapname") {
+                similarityAttribute = mapCorrelation[baseCaseValue]?.[itCaseValue] || 0;
+            } else if (attribute === "operator") {
+                similarityAttribute = getOperatorCorrelation(baseCaseValue, itCaseValue);
+            } else {
+                similarityAttribute = baseCaseValue === itCaseValue ? 1 : 0;
+            }
+    
+            totalSimilarity += weight * similarityAttribute;
         }
-
-        totalSimilarity += weight * similarityAttribute;
     });
 
     return totalSimilarity;
